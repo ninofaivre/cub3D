5c93844d0daa79a7b432e1b253983072a9848dd7
paulochon1202 <paul.boutelliez12@gmail.com>
2022-02-27 18:37:18 +0100

    push du parsing

--- src/parsing.c
+++ src/parsing.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nfaivre <nfaivre@student.42.fr>            +#+  +:+       +#+        */
+/*   By: paboutel <paboutel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 17:42:12 by nfaivre           #+#    #+#             */
-/*   Updated: 2022/02/26 18:11:21 by nfaivre          ###   ########.fr       */
+/*   Updated: 2022/02/27 18:25:00 by paboutel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,14 @@ char	put_string_struc(char *str, int i)
 		new_str[j] = str[i + j];
 		j--;
 	}
+	j = open(str, O_RDONLY);
+	if (j == -1)
+	{
+		print_error("Wrong texture's PATH/NAME \n");
+		close (j);
+		return (NULL);
+	}
+	close(j); 
 	return (new_str);
 }
 
@@ -53,31 +61,42 @@ bool	pars_texture_info(char *str, int i, t_conf *conf)
 	{
 		i = skip_space(str, ++i);
 		conf->NO = put_string_struct(str, i);
+		if (!conf->NO)
+			return (false);
 	}
 	else if (str[i] == 'S' && str[i + 1] == 'O')
 	{
 		i = skip_space(str, ++i);
 		conf->SO = put_string_struct(str, i);
+		if (!conf->SO)
+			return (false);
 	}
 	else if (str[i] == 'W' && str[i + 1] == 'E')
 	{
 		i = skip_space(str, ++i);
 		conf->WE = put_string_struct(str, i);
+		if (!conf->WE)
+			return (false);
 	}
 	else if (str[i] == 'E' && str[i + 1] == 'A')
 	{
 		i = skip_space(str, ++i);
 		conf->EA = put_string_struct(str, i);
+		if (!conf->EA)
+			return (false);
 	}
 	else if (str[i] == 'S' && str[i + 1] == ' ')
 	{
 		i = skip_space(str, i++);
 		conf->S = put_string_struct(str, i);
+		if (!conf->S)
+			return (false);
 	}
 	else
 		return (false);
 	return (true);
 }
+
 int	fake_atoi(char *str, int i)
 {
 	int	nb;
@@ -101,34 +120,38 @@ int		next_nb(char *str, int i)
 	return (i);
 }
 
-bool	pars_RGB(char *str, int i, t_conf *conf)
+bool	pars_RGB(char *str, int i, t_rgb *rgb)
 {
-	if (str[i] == 'F')
+	int error;
+
+	error = 0;
+	i = skip_space(str, ++i);
+	if (rgb->r == -1)
+		rgb->r = fake_atoi(str, i);
+	else
+		error++;
+	i = next_nb(str, i);
+	if (rgb->g == -1)
+		rgb->g = fake_atoi(str, i);
+	else
+		error++;
+	i = next_nb(str, i);
+	if (rgb->b == -1)
+		rgb->b = fake_atoi(str, i);
+	else
+		error++;
+	if (error > 0)
 	{
-		i = skip_space(str, ++i);
-		conf->F->R = fake_atoi(str, i);
-		i = next_nb(str, i);
-		conf->F->G = fake_atoi(str, i);
-		i = next_nb(str, i);
-		conf->F->B = fake_atoi(str, i);
-		if (conf->F->R < 0 || conf->F->R > 255 || conf->F->G < 0 || conf->F->G > 255 || conf->F->B < 0 || conf->F->B > 255)
-			return (false);
-		else
-			return (true);
+		print_error("too much F or C\n");
+		return (false);
 	}
-	if (str[i] == 'C')
+	if (rgb->r < 0 || rgb->r > 255 || rgb->g < 0 || rgb->g > 255 || rgb->b < 0 || rgb->b > 255)
 	{
-		i = skip_space(str, ++i);
-		conf->C->R = fake_atoi(str, i);
-		i = next_nb(str, i);
-		conf->C->G = fake_atoi(str, i);
-		i = next_nb(str, i);
-		conf->C->B = fake_atoi(str, i);
-		if (conf->C->R < 0 || conf->C->R > 255 || conf->C->G < 0 || conf->C->G > 255 || conf->C->B < 0 || conf->C->B > 255)
-			return (false);
-		else
-			return (true);
+		print_error("wrong rgb\n");
+		return (false);
 	}
+	else
+		return (true);	
 }
 
 bool	pars_string_info(char *str, t_conf *conf)
@@ -139,15 +162,12 @@ bool	pars_string_info(char *str, t_conf *conf)
 	i = skip_space(str, i);
 	if (str[i] == 'N'|| str[i] == 'S' || str[i] == 'W' || str[i] == 'E')
 		return (pars_texture_info(str, i, conf));
-	else if (str[i] == 'F' || str[i] == 'C')
-		return (pars_RGB(str, i, conf));
-	else if (str[i] == '\n' || str[i] == '\0')
+	else if (str[i] == 'F')
+		return (pars_RGB(str, i, &conf->F));
+	else if (str[i] == 'C')
+		return (pars_RGB(str, i, &conf->C));
+	else if (str[i] == '\n')
 		return (true);
-	else if (str[i] == '1')
-	{
-		add_str_to_str_tab(conf->map, str);
-		return (true);
-	}
 	else
 		return (false);
 }
